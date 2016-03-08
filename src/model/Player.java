90765b06089b73338910f3325c270eb88d68acd5
swammer5 <swammer@cs.washington.edu>
2016-03-07 16:53:40 -0800

    Added skeleton Player

---
+++ src/model/Player.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ * <b>Player</b> is a data encapulation class that represents the player in the
+ * game.
+ * 
+ * @author swammer
+ */
+public class Player {
+
+	String name;
+	// TODO: add other fields
+	
+	public Player(String name) {
+		this.name = name;
+		// TODO: instantiate other fields
+	}
+}
