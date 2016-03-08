7553b22ffa24dddd09e510d8211f47e169c1c3c1
swammer5 <swammer@cs.washington.edu>
2016-03-07 18:02:30 -0800

    Added skeleton GameMain

---
+++ src/main/GameMain.java
@@ -0,0 +1,74 @@
+package main;
+
+import java.util.Scanner;
+
+import model.GameModel;
+
+public class GameMain {
+
+	private static GameModel model;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to your very own adventure!");
+		
+		Scanner in = new Scanner(System.in);
+		// You might ask for the save file here
+		setup();
+		
+		interact(in);
+		System.exit(0);
+	}
+
+	/**
+	 * Sets up the GameModel to prepare the game.
+	 */
+	private static void setup() {
+		// we may want to accept a file number here to load a particular save
+		model = new GameModel();
+	}
+
+	/**
+	 * Handles main interaction with user.
+	 */
+	private static void interact(Scanner in) {
+		boolean quit = false;
+		while (!quit) {
+			// TODO ask for input and redirect to appropriate command method
+			String line = in.nextLine().toLowerCase().trim();
+			if (!line.equals("")) {
+				String[] tokens = line.split(" ");
+				String command = tokens[0]; // TODO: test split on empty string
+				if (command.equals("m") || command.equals("menu") ||
+						command.equals("help") || command.equals("h") || 
+						command.equals("?")) {
+					in.nextLine();
+					menu();
+				} else if (command.equals("q")) {
+	          /*
+				} else if () {
+					
+				} else if () {
+					
+				} else if () {
+					
+				} else if () {
+	          */
+				} else {
+					System.out.println("Command not recognized. Try \"menu,\""
+							+ "\"help,\" or \"?\" to see valid commands.");
+				}
+			}
+		}
+	}
+
+	/**
+	 * Displays the menu of available commands to the user
+	 */
+	private static void menu() {
+		System.out.println("Menu");
+		System.out.println("====");
+		System.out.println("\t(m)enu or (h)elp or ? - displays this menu");
+		// TODO: rest of the commands
+	}
+}
