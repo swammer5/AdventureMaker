13143ba608ab76c46b03e6979db60e450aa5cb48
swammer5 <swammer@cs.washington.edu>
2016-03-12 22:32:46 -0800

    Added GameState skeleton class

---
+++ src/model/GameState.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * <b>GameState</b> represents the current game data. GameState can be
+ * mutated to reflect changes in the game. GameState will keep track of all
+ * relevant details about the game.
+ *  
+ * @author Sean Wammer
+ */
+public class GameState {
+
+}
