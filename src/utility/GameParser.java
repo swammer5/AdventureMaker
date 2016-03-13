45b5922fcda677812dc0941cfa031ae646d4a644
swammer5 <swammer@cs.washington.edu>
2016-03-12 23:50:45 -0800

    Added loadRoom stubbed method to GameParser

--- src/utility/GameParser.java
+++ src/utility/GameParser.java
@@ -2,12 +2,13 @@ package utility;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import model.GameState;
+import model.Room;
 
 /**
  * <b>GameParser</b> is a utility class that handles loading game data from files
  * on disc and populating useful Java objects.
  * 
- * @author swammer
+ * @author Sean Wammer
  */
 public class GameParser {
 
@@ -15,5 +16,7 @@ public class GameParser {
 		throw new NotImplementedException();
 	}
 	
-	public static 
+	public static Room loadRoom(String filePath) {
+		throw new NotImplementedException();
+	}
 }
