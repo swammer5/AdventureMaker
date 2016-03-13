6dbf720bd9ec680a845027dfeb751003bb41ba49
swammer5 <swammer@cs.washington.edu>
2016-03-13 00:05:02 -0800

    Added GameSaver skeleton file.

---
+++ src/utility/GameSaver.java
@@ -0,0 +1,22 @@
+package utility;
+
+import model.GameState;
+import model.Room;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * <b>GameSaver</b> is a utility class that handles writing game data to files
+ * on disc that can later be loaded by GameParser.
+ * 
+ * @author Sean Wammer
+ */
+public class GameSaver {
+
+	public static GameState saveGameState(String filePath) {
+		throw new NotImplementedException();
+	}
+	
+	public static Room saveRoom(String filePath) {
+		throw new NotImplementedException();
+	}
+}
