0d7b8f93a14ec5b3159a0f8a9f3529cc3f14f4f3
swammer5 <swammer@cs.washington.edu>
2016-03-12 23:49:48 -0800

    Added GameParser skeleton file.

---
+++ src/utility/GameParser.java
@@ -0,0 +1,19 @@
+package utility;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import model.GameState;
+
+/**
+ * <b>GameParser</b> is a utility class that handles loading game data from files
+ * on disc and populating useful Java objects.
+ * 
+ * @author swammer
+ */
+public class GameParser {
+
+	public static GameState loadGameState(String filePath) {
+		throw new NotImplementedException();
+	}
+	
+	public static 
+}
