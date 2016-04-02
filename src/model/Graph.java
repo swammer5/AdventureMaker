55654cb48b9856f7e997548e65e05ce81436822b
swammer5 <swammer@cs.washington.edu>
2016-04-02 00:06:40 -0700

    Added Graph skeleton

---
+++ src/model/Graph.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ * Graph represents a mutable, directed, non-labeled, non-multigraph. Edges of
+ * Graph are one way, there are no edge labels, and edges are unique. Graphs
+ * can consist of a number of Nodes connected by Edges.
+ * 
+ * Nodes hold any objects of type K. All objects in graph of type K in nodes
+ * must be unique, that is, for two objects in this i,j i.equals(j) must be
+ * false.
+ * 
+ * @author Sean Wammer
+ * 
+ * @param <K> the type of the data that Node stores
+ */
+public class Graph<K> {
+	// not yet implemented!
+}
