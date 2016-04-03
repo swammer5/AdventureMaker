e5e33cc60665c02a48a6038d84f4023ce34c40f5
swammer5 <swammer@cs.washington.edu>
2016-04-03 14:47:42 -0700

    Added adjacentRooms for GameState

--- src/model/GameState.java
+++ src/model/GameState.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * <b>GameState</b> represents the current game data. GameState can be mutated
  * to reflect changes in the game. GameState will keep track of all relevant
@@ -9,7 +12,7 @@ package model;
  */
 public class GameState {
 
-    Graph<Room> rooms;
+    private Graph<Room> rooms;
     private Player player;
     private Room currentPlayerRoom;
 
@@ -23,5 +26,20 @@ public class GameState {
         this.rooms = rooms;
         this.player = player;
         this.currentPlayerRoom = currentPlayerRoom;
+    }
+
+    /**
+     * Returns all the short names of the rooms the the player can travel to
+     * from their current room. Returns an empty set if there are no paths
+     * leading from this room.
+     */
+    public Set<String> adjacentRooms() {
+        Set<String> availableRooms = new HashSet<>();
+
+        for (Room room : rooms.adjacent(currentPlayerRoom)) {
+            availableRooms.add(room.shortName());
+        }
+
+        return availableRooms;
     }
 }
