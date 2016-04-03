387264050a83271788c65f6a959406959cbd1046
swammer5 <swammer@cs.washington.edu>
2016-04-03 01:48:07 -0700

    Added constructor to GameState

--- src/model/GameState.java
+++ src/model/GameState.java
@@ -1,12 +1,27 @@
 package model;
 
 /**
- * <b>GameState</b> represents the current game data. GameState can be
- * mutated to reflect changes in the game. GameState will keep track of all
- * relevant details about the game.
- *  
+ * <b>GameState</b> represents the current game data. GameState can be mutated
+ * to reflect changes in the game. GameState will keep track of all relevant
+ * details about the game.
+ * 
  * @author Sean Wammer
  */
 public class GameState {
 
+    Graph<Room> rooms;
+    private Player player;
+    private Room currentPlayerRoom;
+
+    /*
+     * Constructs a new GameState with the given data.
+     * 
+     * In general, parser utility classes should load this data and construct a
+     * new GameState
+     */
+    public GameState(Graph<Room> rooms, Player player, Room currentPlayerRoom) {
+        this.rooms = rooms;
+        this.player = player;
+        this.currentPlayerRoom = currentPlayerRoom;
+    }
 }
