6f075bd404875814d4afe1b02c9db631ddb43001
swammer5 <swammer@cs.washington.edu>
2016-04-03 14:49:15 -0700

    Added adjacentRooms for GameModel

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
@@ -8,37 +11,51 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  * @author Sean Wammer
  */
 public class GameModel {
-	
-	private GameState gameState;
-	
-	/**
-	 * Constructs a new GameModel
-	 */
-	public GameModel() {
-		// TODO: instantiate fields
-		
-		// when we want to load from a file, we won't make a new GameState,
-		// we will call GameParser.loadGameState(filepath) to return a populated
-		// GameState
-		GameState gameState = new GameState();
-	}
-	
-	/**
-	 * Returns true if and only if the command was recognized by the room.
-	 * Runs the script associated with the command in this room.
-	 * 
-	 * @param command the command to attempt to run in this room
-	 * @return true iff the command was recognized and run
-	 */
-	public boolean execute(String command) {
-		throw new NotImplementedException();
-	}
-	
-	public boolean go(String roomName) {
-		throw new NotImplementedException();
-	}
-	
-	public void look() {
-		throw new NotImplementedException();
-	}
+
+    private GameState gameState;
+
+    /**
+     * Constructs a new GameModel
+     */
+    public GameModel() {
+        // TODO: instantiate fields
+
+        // when we want to load from a file, we won't make a new GameState,
+        // we will call GameParser.loadGameState(filepath) to return a populated
+        // GameState
+        // the code below is subject to change:
+        // String filepath = "saves/save" + fileNum + ".tsv";
+        // GameState gameState = GameParser.loadGameState(filepath)
+    }
+
+    /**
+     * Returns true if and only if the command was recognized by the room. Runs
+     * the script associated with the command in this room.
+     * 
+     * @param command the command to attempt to run in this room
+     * @return true iff the command was recognized and run
+     */
+    public boolean execute(String command) {
+        throw new NotImplementedException();
+    }
+
+    public boolean go(String roomName) {
+        throw new NotImplementedException();
+    }
+
+    public void look() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns the short names of all the rooms the player can travel to from
+     * their current location. Returns an empty set if there are no paths
+     * leading from this room.
+     * 
+     * @return all the short names of all the rooms the player can travel to
+     *         from their current location
+     */
+    public Set<String> adjacentRooms() {
+        return gameState.adjacentRooms();
+    }
 }
