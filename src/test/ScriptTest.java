86c4ac3120ee14930a3b97dfacd57747e274d052
swammer5 <swammer@cs.washington.edu>
2016-04-26 12:39:54 -0700

    ScriptTest skeleton file added

---
+++ src/test/ScriptTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class ScriptTest extends TestCase {
+
+    public void testAppend() {
+        fail("Not yet implemented");
+    }
+    
+    public void testExecute() {
+        fail("Not yet implemented");
+    }
+    
+    // TODO: fill in more tests when Script is fleshed out
+}
