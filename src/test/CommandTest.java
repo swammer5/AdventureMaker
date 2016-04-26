3bfa3639897991d7aff7e916378943a6a50021c9
swammer5 <swammer@cs.washington.edu>
2016-04-26 15:40:22 -0700

    Added CommandTest skeleton file

---
+++ src/test/CommandTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class CommandTest extends TestCase {
+
+	public void testPrint() {
+        fail("Not yet implemented");
+    }
+	
+	public void testAddHealth() {
+	    fail("Not yet implement");
+	}
+	
+	// TODO: fill in more tests when Command and CommandType is fleshed out
+}
