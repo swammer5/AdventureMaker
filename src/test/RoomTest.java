0f156952c9619f8c3df82e8414aaa53fbbe7c6cb
swammer5 <swammer@cs.washington.edu>
2016-04-26 20:06:49 -0700

    Added RoomTest skeleton file

---
+++ src/test/RoomTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class RoomTest extends TestCase {
+
+	public void testAddScript() {
+		fail("Not yet implemented");
+	}
+	
+	public void testRemoveScript() {
+	    fail("Not yet implemented");
+	}
+	
+	public void testHasCommand() {
+	    fail("Not yet implemented");
+	}
+	
+	public void testExecuteBadCommand() {
+	    fail("Not yet implemented");
+	}
+	
+	public void testExecute() {
+	    fail("Not yet implemented");
+	}
+}
