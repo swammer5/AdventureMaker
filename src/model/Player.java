22cf9567d5fddd6a3d5ee0e0df9661db17a6946e
swammer5 <swammer@cs.washington.edu>
2016-04-27 01:31:54 -0700

    Implemented most of Player

--- src/model/Player.java
+++ src/model/Player.java
@@ -8,20 +8,99 @@ package model;
  */
 public class Player {
 
-	String name;
-	// TODO: add other fields
-	
-	public Player(String name) {
-		this.name = name;
-		// TODO: instantiate other fields
-	}
-	
-	/**
-	 * Returns the name of this
-	 * 
-	 * @return the name of this
-	 */
-	public String name() {
-		return name;
-	}
+    private String name;
+    private int curHealth;
+    private int maxHealth;
+
+    // TODO: add other fields
+
+    public Player(String name, int curHealth, int maxHealth) {
+        this.name = name;
+        this.curHealth = curHealth;
+        this.maxHealth = maxHealth;
+
+        // TODO: instantiate other fields
+    }
+
+    /**
+     * Returns the name of this
+     * 
+     * @return the name of this
+     */
+    public String name() {
+        return name;
+    }
+
+    /**
+     * Returns the current health of this
+     * 
+     * @return the current health of this
+     */
+    public int curHealth() {
+        return curHealth;
+    }
+
+    /**
+     * Returns the max health of this
+     * 
+     * @return the max health of this
+     */
+    public int maxHealth() {
+        return maxHealth;
+    }
+
+    /**
+     * Adds the given health (can be negative) to this.
+     * 
+     * @param health the amount to change the player's current health by
+     */
+    public void addHealth(int health) {
+        setCurHealth(curHealth + health);
+    }
+
+    /**
+     * Increase the maximum health of this player by the given amount. Or
+     * decrease if the given amount is negative.
+     * 
+     * @param health
+     */
+    public void addMaximumHealth(int health) {
+        setMaxHealth(maxHealth + health);
+    }
+
+    /**
+     * Changes the name of this to the given name
+     * 
+     * @param name the new name of this
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Changes the current health of this to the given int
+     * 
+     * @param health the new current health to set this at
+     */
+    public void setCurHealth(int health) {
+        curHealth = health;
+    }
+
+    /**
+     * Changes the max health of this to the given int
+     * 
+     * @param health the new max health to set this at
+     */
+    public void setMaxHealth(int health) {
+        maxHealth = health;
+    }
+
+    /**
+     * Returns true if and only if this player's current health is zero or less.
+     * 
+     * @return true iff this player's current health is zero or less
+     */
+    public boolean isDead() {
+        return curHealth <= 0;
+    }
 }
