ef251f4297238dd5993e5e0013409a07d18251a4
swammer5 <swammer@cs.washington.edu>
2016-04-26 20:31:30 -0700

    Added GameStateTest skeleton file

---
+++ src/test/GameStateTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class GameStateTest extends TestCase {
+
+	public void testAdjacentRooms() {
+		fail("Not yet implemented");
+	}
+	
+	public void testAddHealth() {
+	    fail("Not yet implemented");
+	}
+	
+	public void testGo() {
+	    fail("Not yet implemented");
+	}
+	
+	public void testSetRoomDesc() {
+	    fail("Not yet implemented");
+	}
+}
