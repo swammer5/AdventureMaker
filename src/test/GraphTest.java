3bea9be688886ab8c6b447e080554f48164d0937
swammer5 <swammer@cs.washington.edu>
2016-04-26 20:12:06 -0700

    Added GraphTest skeleton file

---
+++ src/test/GraphTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class GraphTest extends TestCase {
+
+	public void testAddEdge() {
+		fail("Not yet implemented");
+	}
+	
+	public void testRemoveEdge() {
+	    fail("Not yet implemented");
+	}
+	
+	public void testAddNode() {
+	    fail("Not yet implemented");
+	}
+	
+	public void testRemoveNode() {
+	    fail("Not yet implemented");
+	}
+	
+	public void testAdjacent() {
+	    fail("Not yet implemented");
+	}
+	
+	public void testAdjacentMissingNode() {
+	    fail("Not yet implemented");
+	}
+}
