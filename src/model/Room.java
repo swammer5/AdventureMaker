83c8fef30285e437cac2e262ad5968c92ba80ac7
swammer5 <swammer@cs.washington.edu>
2016-05-19 12:46:18 -0700

    Implemented getters and constructors for Room.

--- src/model/Room.java
+++ src/model/Room.java
@@ -16,28 +16,66 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class Room {
 
     /** Acts as the identifier for this room. Should never be modified */
-    private String shortName;
+    private final String shortName;
 
     /** mutable parts of this room, can all be changed with setter methods */
     private String name;
     private String shortDesc;
     private String longDesc;
     private List<String> items;
-    private Map<String, Script> acceptedCommands;
+    private Map<String, Script> acceptedInput;
 
+    private static final String DEFAULT_STRING = "...";
+    
     public Room(String shortName) {
-        this(shortName, shortName, "...", "...", new ArrayList<String>(), new HashMap<String,Script>());
+        this(shortName, shortName, DEFAULT_STRING, DEFAULT_STRING);
     }
 
+    public Room(String shortName, String name, String shortDesc, String longDesc) {
+        this(shortName, name, shortDesc, longDesc, new ArrayList<String>(),
+                new HashMap<String, Script>());
+    }
+    
+    /**
+     * Constructs a new Room with the given parameters.
+     * 
+     * @param shortName the unique identifier of this
+     * @param name the descriptive or long name of this
+     * @param shortDesc short description of this
+     * @param longDesc long description of this
+     * @param items a list of items currently in this room
+     * @param acceptedInput a map from accepted inputs to Scripts to execute
+     * @throws IllegalArgumentException if shortName is null 
+     */
     public Room(String shortName, String name, String shortDesc,
             String longDesc, List<String> items,
-            Map<String, Script> acceptedCommands) {
+            Map<String, Script> acceptedInput) {
         this.shortName = shortName;
         this.name = name;
         this.shortDesc = shortDesc;
         this.longDesc = longDesc;
         this.items = items;
-        this.acceptedCommands = acceptedCommands;
+        this.acceptedInput = acceptedInput;
+        
+        // ensure good room representation with null checks
+        if (shortName == null) {
+            throw new IllegalArgumentException("short name must not be null.");
+        }
+        if (name == null) {
+            name = DEFAULT_STRING;
+        }
+        if (shortDesc == null) {
+            shortDesc = DEFAULT_STRING;
+        }
+        if (longDesc == null) {
+            longDesc = DEFAULT_STRING;
+        }
+        if (items == null) {
+            items = new ArrayList<String>();
+        }
+        if (acceptedInput == null) {
+            acceptedInput = new HashMap<String, Script>();
+        }
     }
 
     /**
@@ -50,7 +88,60 @@ public class Room {
         return shortName;
     }
 
-    // TODO add other getter methods
+    /**
+     * Returns the long (descriptive) name of this room.
+     * 
+     * @return the long name of this room.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the short description of this room.
+     * 
+     * @return the short description of this room.
+     */
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    /**
+     * Returns the long description of this room.
+     * 
+     * @return the long description of this room.
+     */
+    public String getDesc() {
+        return longDesc;
+    }
+
+    /**
+     * Returns a list of String representing all the items in this room.
+     * 
+     * (We use a list so we can have duplicates. More precisely, we are
+     * returning an abstract bag of items)
+     * 
+     * @return a list of items in this room.
+     */
+    public List<String> getItems() {
+        List<String> retList = new ArrayList<>();
+        for (String item : items) {
+            retList.add(item);
+        }
+        return retList;
+    }
+
+    /**
+     * Returns the Script associated with the given input for this room or null
+     * if there is no such Script.
+     * 
+     * @param input the user command to retrieve the Script of
+     * @return the Script associated with the given input or null if there is no
+     *         script associated with this input.
+     */
+    public Script getScript(String input) {
+        return acceptedInput.get(fix(input));
+    }
 
     // TODO add setter methods too
 
@@ -69,10 +160,10 @@ public class Room {
      */
     public String execute(String input) {
         input = fix(input);
-        if (!hasCommand(input)) {
+        if (!acceptsInput(input)) {
             return null;
         } else {
-            return acceptedCommands.get(input).execute();
+            return acceptedInput.get(input).execute();
         }
     }
 
@@ -82,13 +173,15 @@ public class Room {
      * with the given command. Given input is not case-sensitive. Room will not
      * remember the case of the command.
      * 
+     * If this room acceptsInput(input), the old script will be returned or null
+     * otherwise.
+     * 
      * @param input the user command to register that triggers the given script
      * @param script the script to add to this room
      */
-    public void addScript(String input, Script script) {
+    public Script addScript(String input, Script script) {
         input = fix(input);
-        // TODO implement
-        throw new NotImplementedException();
+        return acceptedInput.put(input, script);
     }
 
     /**
@@ -97,19 +190,7 @@ public class Room {
      * @param input the user command associated with the Script to remove
      */
     public void removeScript(String input) {
-        acceptedCommands.remove(fix(input));
-    }
-
-    /**
-     * Returns the Script associated with the given input for this room or null
-     * if there is no such Script.
-     * 
-     * @param input the user command to retrieve the Script of
-     * @return the Script associated with the given input or null if there is no
-     *         script associated with this input.
-     */
-    public Script getScript(String input) {
-        return acceptedCommands.get(input);
+        acceptedInput.remove(fix(input));
     }
 
     /**
@@ -119,13 +200,12 @@ public class Room {
      * @param input the user command to check
      * @return true iff this room has the given input registered to a script
      */
-    public boolean hasCommand(String input) {
-        return acceptedCommands.containsKey(fix(input));
+    public boolean acceptsInput(String input) {
+        return acceptedInput.containsKey(fix(input));
     }
 
     @Override
     public int hashCode() {
-        // TODO implement
         // IMPLEMENTATION NOTE, HASH CODE CAN ONLY DEPEND ON THE *IMMUTABLE*
         // PARTS OF THIS ROOM, SO THAT WE CAN SET ALL THESE FIELDS LATER, AND IT
         // WON'T VIOLATE THE STATE OF THE GRAPH BY RETURNING DIFFERENT HASH
@@ -157,7 +237,7 @@ public class Room {
      * Standardizes the input so that we are more likely to recognize commands.
      * 
      * Implementation note: We currently flatten to lower case and trim
-     * whitespace.
+     * whitespace, so 'get axe' will be the same as ' Get Axe ' and ' GEt AxE'
      * 
      * @param input the input String to standardize
      * @return the standardized input string
