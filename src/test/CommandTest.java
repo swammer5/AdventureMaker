ce6ae1268a869f8777802f166a56e6a3cddca8c5
swammer5 <swammer@cs.washington.edu>
2016-05-21 00:09:43 -0700

    Added a CommandTest unit test.

--- src/test/CommandTest.java
+++ src/test/CommandTest.java
@@ -1,25 +1,41 @@
 package test;
 
 import static org.junit.Assert.*;
+import model.Command;
+import model.CommandType;
+import model.GameModel;
+
 import org.junit.Test;
 import org.junit.BeforeClass;
 
 public class CommandTest {
 
+    private static GameModel model;
+
     @BeforeClass
     public static void setup() {
-    
+        // this should be mocked but it's ok. It'll just depend on GameModel.
+        model = GameModel.newGame();
     }
-    
+
     @Test
-	public void testPrint() {
-        fail("Not yet implemented");
+    public void testPrint() {
+        String expected = "Print works!";
+
+        String[] args = { expected };
+        Command com = new Command(model, CommandType.PRINT, args);
+
+        String actual = com.execute();
+
+        assertEquals("PRINT should return the given String", expected, actual);
     }
-	
+
     @Test
-	public void testAddHealth() {
-	    fail("Not yet implement");
-	}
-	
-	// TODO: fill in more tests when Command and CommandType is fleshed out
+    public void testAddHealth() {
+        // need to check that gameModel says player's health is changed by the
+        // expected ammount.
+        fail("Not yet implement");
+    }
+
+    // TODO: fill in more tests when Command and CommandType is fleshed out
 }
