3e24aae82ae5135eee21c16ff80d8b77ebe48ae1
swammer5 <swammer@cs.washington.edu>
2016-05-24 01:27:46 -0700

    Added CommandTest testAddHealth.
    This relies on the specification that player health is not enforced
    (capped) by max health. We may want to rethink this in the future. If we
    make it so health caps at max health, we need to change this unit test,
    and we should expect it to fail under certain circumstances.

--- src/test/CommandTest.java
+++ src/test/CommandTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import model.Command;
 import model.CommandType;
 import model.GameModel;
+import model.Player;
 
 import org.junit.Test;
 import org.junit.BeforeClass;
@@ -34,8 +35,33 @@ public class CommandTest {
     public void testAddHealth() {
         // need to check that gameModel says player's health is changed by the
         // expected ammount.
-        fail("Not yet implement");
-    }
 
+        String[] args = { "5" };
+        Command com = new Command(model, CommandType.ADD_HEALTH, args);
+
+        Player player = model.getGameState().getPlayer();
+        int prevHealth = player.curHealth();
+        int prevMaxHealth = player.maxHealth();
+
+        // test output
+        String actual = com.execute();
+        String expected = "";
+        assertEquals("ADD_HEALTH should return empty string", expected, actual);
+
+        // first sanity check that player and player2 both refer to the same
+        // object. This isn't where the test should go, but we'll test this in
+        // gameState too.
+        Player player2 = model.getGameState().getPlayer();
+        assertEquals("gameState should return reference to player", player,
+                player2);
+        
+        // test effects of command
+        int newHealth = player2.curHealth();
+        int newMaxHealth = player2.maxHealth();
+        int expected2 = prevHealth + 5;
+        int actual2 = newHealth;
+        assertEquals("health should be updated by ADD_HEALTH", expected2, actual2);
+        assertEquals("max health should stay the same", prevMaxHealth, newMaxHealth);
+    }
     // TODO: fill in more tests when Command and CommandType is fleshed out
 }
